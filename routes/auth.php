f8f122ebbc45515a597b1461f1f0006d8d1854b4
AlumG <AlumG@majada.es>
2024-11-15 21:51:13 +0100

    fb routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\FirebaseAuthController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
@@ -13,8 +12,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [FirebaseAuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('register', [FirebaseAuthController::class, 'register']);
     //Route::get('register', [RegisteredUserController::class, 'create'])
     // ->name('register');
     //Route::post('register', [RegisteredUserController::class, 'store']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\FirebaseAuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('register', [FirebaseAuthController::class, 'showRegisterForm'])->name('register');
+Route::post('register', [FirebaseAuthController::class, 'register']);
 
 Route::get('/', function () {
     return view('welcome');
