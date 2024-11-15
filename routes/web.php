45f359c6a07467fa77a53ac5ff1759ed73df1499
AlumG <AlumG@majada.es>
2024-11-15 21:20:32 +0100

    added firebase to routes/web

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\FirebaseAuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', [FirebaseAuthController::class, 'showRegisterForm']);
+
+Route::post('register', [FirebaseAuthController::class, 'register']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +22,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
