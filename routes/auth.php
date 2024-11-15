d95518cc2f7677f3afe78b6e2258a06dd7aa1385
AlumG <AlumG@majada.es>
2024-11-15 21:44:39 +0100

    firebase routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FirebaseAuthController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
@@ -12,10 +13,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::get('register', [FirebaseAuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('register', [FirebaseAuthController::class, 'register']);
+    //Route::get('register', [RegisteredUserController::class, 'create'])
+    // ->name('register');
+    //Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
