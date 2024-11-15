4aae8f6504a17fc6bcfea99c021558d5bc0cc117
AlumG <AlumG@majada.es>
2024-11-15 21:32:15 +0100

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboarddddddddd') }}
         </h2>
     </x-slot>
 
